c72715036a10e207c9640a452303245baed810fc
juscius <justas.vasiljevas@gmail.com>
2023-03-04 20:11:23 +0200

    update

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p9el3(3dy#3#kpxoyxda_abx68g&4agcf@^_yn5jtic6)%swi9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- learning_log/wsgi.py
+++ learning_log/wsgi.py
@@ -15,4 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning_log.settings')
 
 application = get_wsgi_application()
 
-app = application
+
